bf274072df04ace981489ae99eb41736bb7006d8
unknown <james.schuepbach@gmail.com>
2013-11-01 17:20:55 +0100

    working on gamehelperClass

--- src/connect4/GameHelper.java
+++ src/connect4/GameHelper.java
@@ -3,18 +3,43 @@ package connect4;
 public class GameHelper {
 
 	/**
-	 * Check if the a Player win
-	 * Return the Number of the Player who wins the game
-	 * or 0 (noboy wins)
+	 * Check if the specifc player wins the game
 	 * @param board
 	 * @return
 	 */
-	public static int checkWinCondition(int[][] board)
+	public static boolean hasPlayerWon(int[][] board,int player)
 	{
-		for(int y =0; y < Game.ROWS;y++)
+		/*
+		 * check all horizontal condition
+		 * X X X X 
+		 */
+		for(int y = 0; y < Game.ROWS; y++)
 		{
-			
+			for(int x =0; x < Game.COLS-4;x++)
+			{
+				if(board[x][y] == player && board[x+1][y] == player 
+				  && board[x+2][y] == player && board[x+3][y] == player)
+					return true;
+			}
 		}
-		return 0;
+		/*
+		 * check vertical condition
+		 * X
+		 * X
+		 * X
+		 * X
+		 */
+		for(int y = 0; y < Game.ROWS -4;y++)
+		{
+			for(int x = 0; x < Game.COLS; x++)
+			{
+				if(board[x][y] == player && board[x][y+1] == player &&
+					board[x][y+2] == player && board[x][y+3] == player)
+					return true;
+			}
+		}
+		
+		
+		return false;
 	}
 }
