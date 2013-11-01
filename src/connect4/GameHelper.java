727cce67f9310ff9063f5f2f9e1091a9f82eac31
unknown <james.schuepbach@gmail.com>
2013-11-01 14:51:12 +0100

    added gameHelper class

---
+++ src/connect4/GameHelper.java
@@ -0,0 +1,20 @@
+package connect4;
+
+public class GameHelper {
+
+	/**
+	 * Check if the a Player win
+	 * Return the Number of the Player who wins the game
+	 * or 0 (noboy wins)
+	 * @param board
+	 * @return
+	 */
+	public static int checkWinCondition(int[][] board)
+	{
+		for(int y =0; y < Game.ROWS;y++)
+		{
+			
+		}
+		return 0;
+	}
+}
