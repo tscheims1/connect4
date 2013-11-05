09a7746247e21c679cb1178bdb40153f8f45a093
unknown <james.schuepbach@gmail.com>
2013-11-05 17:49:41 +0100

    added winCondition in gameHelper Class

--- src/connect4/GameHelper.java
+++ src/connect4/GameHelper.java
@@ -38,7 +38,45 @@ public class GameHelper {
 					return true;
 			}
 		}
-		
+		/*
+		 *
+		 * check diagonal condition1
+		 *    X
+		 *   X 
+		 *  X
+		 * X
+		 */ 
+		 for(int y = 0; y < Game.ROWS-4; y++)
+		 {
+			 for(int x = 0; x < Game.COLS-4;x++)
+			 {
+				 if(board[x][y] == player && board[x+1][y+1] == player && 
+						 board[x+2][y+2] == player && board[x+3][y+3] == player)
+				 {
+					 return true;
+				 }
+						 
+			 }
+		 }
+		 /*
+		  * check diagonal condition2
+		  * X
+		  *   X
+		  *     X
+		  *      X 
+		  */
+		for(int y = Game.ROWS-1; y-4 < 0;y++ )
+		{
+			
+			for(int x = 0; x < Game.ROWS-4;  x++)
+			{
+				if(board[x][y] == player && board[x+1][y-1] == player &&
+					board[x+2][y-2] == player && board[x+3][y-3] == player)
+				{
+					return true;
+				}
+			}
+		}
 		
 		return false;
 	}
